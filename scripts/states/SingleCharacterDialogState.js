84a30e439ec1b929688d2019eb237d0cd6378e5a
Kenny McCormick <kennyrkun@users.noreply.github.com>
2024-03-06 10:59:22 -0600

    fix missing imports and wait for character to fade before finishing cleanup in SingleCharacterDialogState

--- scripts/states/SingleCharacterDialogState.js
+++ scripts/states/SingleCharacterDialogState.js
@@ -1,17 +1,19 @@
-import { DialogManager } from "../DialogManager.js";
 import { State } from "./State.js";
+
+import { CharacterManager } from "../CharacterManager.js";
+import { DialogManager } from "../DialogManager.js";
 
 export class SingleCharacterDialogState extends State
 {
     async init(characterId, dialogId, environmentId = null)
     {
-	this.characterManager = new CharacterManager();
-	await this.characterManager.loadCharacters();
+	    this.characterManager = new CharacterManager();
+        await this.characterManager.loadCharacters();
 	    
         this.character = this.characterManager.getCharacter(characterId);
 
         this.dialogManager = new DialogManager();
-	await this.dialogManager.loadDialog();
+	    await this.dialogManager.loadDialog();
 
         this.dialogBox = this.dialogManager.startDialog(dialogId, this.character);
         this.character.show();
