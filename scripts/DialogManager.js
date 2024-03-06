5461fc7b3d618b6db5d20009d6f855c656771693
kennyrkun <kennyrkun@kerrishaus.com>
2024-03-05 20:48:39 -0600

    implement text animation

--- scripts/DialogManager.js
+++ scripts/DialogManager.js
@@ -2,7 +2,12 @@ export class DialogManager
 {
     constructor()
     {
-        this.dialogBoxElement = null;
+        this.dialogBoxElement     = null;
+        this.dialogBoxTextElement = null;
+        this.dialogBoxButton      = null;
+
+        this.textAnimationTime = 100;
+        this.textAnimationInterval = null;
 
         this.character = null;
         this.dialog    = null;
@@ -28,16 +33,44 @@ export class DialogManager
 
         this.clearDialogBox();
 
-        const dialog = this.getDialog(dialogId);
+        this.dialog = this.getDialog(dialogId);
 
-        this.dialogBoxElement = $(`<div class="dialog-box bottom" id="dialogBox"><div class="dialog-owner">${character.name}</div><div class="">${dialog}</div></div>`).appendTo(document.body);
+        this.dialogBoxElement     = $(`<div class="dialog-box bottom" id="dialogBox"><div class="dialog-owner">${character.name}</div></div>`).appendTo(document.body);
+        this.dialogBoxTextElement = $(`<div class="dialog-text"></div>`).appendTo(this.dialogBoxElement);
+        this.dialogBoxButton      = $(`<div id="dialogButtons" class=""><button class="dialog-button">Next</button></div>`).appendTo(this.dialogBoxElement);
 
-        console.log("dialog is ready");
+        this.dialog.currentDialog = 0;
+        this.dialog.tempTextCopy = this.dialog.dialog[this.dialog.currentDialog];
+
+        console.log("dialog is ready", this.dialog);
 
         setTimeout(() =>
         {
             this.dialogBoxElement.addClass("shown");
+
+            this.startTextAnimation();
         }, 0);
+    }
+
+    startTextAnimation()
+    {
+        this.textAnimationInterval = setInterval(() =>
+        {
+            if (this.dialog.tempTextCopy.length > 0)
+            {
+                this.dialogBoxTextElement.text(this.dialogBoxTextElement.text() + this.dialog.tempTextCopy[0]);
+
+                this.dialog.tempTextCopy = this.dialog.tempTextCopy.slice(1);
+
+                return;
+            }
+
+            clearInterval(this.textAnimationInterval);
+            console.log("text animation finished");
+
+            this.dialogBoxButton.addClass("shown");
+            this.dialogBoxButton.focus();
+        }, this.textAnimationTime);
     }
 
     getDialog(dialogId)
