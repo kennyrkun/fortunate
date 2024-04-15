68ed372a090ffe87a14d20eb166e82e79441bb88
Kenny McCormick <kennyrkun@users.noreply.github.com>
2024-04-15 15:19:46 -0500

    add options to dialogbox

--- scripts/DialogManager.js
+++ scripts/DialogManager.js
@@ -46,29 +46,43 @@ export class DialogManager
         this.clearDialogBox();
 
         this.dialog = this.getDialog(dialogId);
-
+        
         this.dialogBoxElement     = $(`<div class="dialog-box bottom" id="dialogBox"><div class="dialog-owner">${character.name}</div></div>`).appendTo(document.body);
         this.dialogBoxTextElement = $(`<div class="dialog-text"></div>`).appendTo(this.dialogBoxElement);
 
-        this.dialogBoxButtonContainer = $(`<div id="dialogButtons" style="display: none;"></div>`).appendTo(this.dialogBoxElement);
-        
-        this.dialogBoxButton = $(`<button class="dialog-button">Next</button>`).appendTo(this.dialogBoxButtonContainer);
-        this.dialogBoxButton.click(() => { this.advanceDialog(); });
-        // TODO: put this on the dialog box itself, not the document
-        // TODO: make this finish the text box instead of immediately sdkipping it
-        $(document).keydown(() => { this.advanceDialog(); });
-
-        this.dialog.currentDialog = 0;
-        this.dialog.tempTextCopy = this.dialog.dialog[this.dialog.currentDialog];
-
-        console.log("dialog is ready", this.dialog);
-
-        setTimeout(() =>
+        if ("option" in this.dialog)
         {
-            this.dialogBoxElement.addClass("shown");
+            foreach (const option of this.dialog.option)
+            {
+                const button = $(`<button class="dialog-button">${option.dialog}</button>`).appendTo(this.dialogBoxButtonContainer);
 
-            this.startTextAnimation();
-        }, 0);
+                if ("next" in option)
+                    button.click(() => { this.startDialog(option.next, 2) );
+                // TODO: button to go to a new scene
+            }
+        }
+        else
+        {
+            this.dialogBoxButtonContainer = $(`<div id="dialogButtons" style="display: none;"></div>`).appendTo(this.dialogBoxElement);
+            
+            this.dialogBoxButton = $(`<button class="dialog-button">Next</button>`).appendTo(this.dialogBoxButtonContainer);
+            this.dialogBoxButton.click(() => { this.advanceDialog(); });
+            // TODO: put this on the dialog box itself, not the document
+            // TODO: make this finish the text box instead of immediately sdkipping it
+            $(document).keydown(() => { this.advanceDialog(); });
+    
+            this.dialog.currentDialog = 0;
+            this.dialog.tempTextCopy = this.dialog.dialog[this.dialog.currentDialog];
+    
+            console.log("dialog is ready", this.dialog);
+    
+            setTimeout(() =>
+            {
+                this.dialogBoxElement.addClass("shown");
+    
+                this.startTextAnimation();
+            }, 0);
+        }
     }
 
     startTextAnimation()
@@ -101,6 +115,8 @@ export class DialogManager
 
             if ("action" in this.dialog)
             {
+                console.log("checking dialog action");
+                
                 // TODO: configure transition time from dialog data
                 const newState = new EnvrionmentNavigationState(this.dialog.action.environment);
 
@@ -113,6 +129,8 @@ export class DialogManager
             }
             else if ("next" in this.dialog)
             {
+                console.log("moving to next dialog" + this.dialog.next);
+                
                 // TODO: don't use this.character.id, instead, it should
                 // use the character designated by the dialog data
                 this.startDialog(this.dialog.next, this.character.id);
