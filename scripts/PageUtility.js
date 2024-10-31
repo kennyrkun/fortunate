f36523aeabd2e3210ea10dee4729fe5b8ad44dea
Kenny McCormick <kennyrmccormick+github@gmail.com>
2024-10-30 23:12:55 -0500

    Create PageUtility.js

---
+++ scripts/PageUtility.js
@@ -0,0 +1,18 @@
+const loadedStyles = new Map();
+
+export function addStyle(filename)
+{
+    const style = document.createElement("link");
+    style.setAttribute("rel", "stylesheet");
+    style.setAttribute("href", `./styles/${filename}.css`);
+    style.setAttribute("id", `${filename}`);
+    document.head.appendChild(style);
+
+    loadedStyles.set(filename, style);
+}
+
+export function removeStyle(filename)
+{
+    loadedStyles.delete(filename);
+    $(`#${filename}`).remove();
+}
