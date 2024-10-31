d07715e8e6fab7c7e63b2ec95a97c64dad02dafe
Kenny McCormick <kennyrkun@users.noreply.github.com>
2024-10-31 11:32:00 -0500

    fix commit title not working?

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 	<head>
 		<title>
 			<?php
-				if (file_exists("./.git"))
+				if (file_exists(".git"))
 				{
-					$refs = file_get_contents("./.git/HEAD");
-					$ref = substr($refs, strrpos($refs, "/"));
-					$commit = file_get_contents("./.git/refs/heads/" . $ref);
+					$refs = file_get_contents(".git/HEAD");
+					$ref = substr($refs, strrpos($refs, "/"), -1); // have to use this to strip a space for some reason
+					$commit = file_get_contents(".git/refs/heads" . $ref); // slash is included in beginning of $ref
 					echo htmlspecialchars($commit);
 				}
 				else
