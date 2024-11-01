8d7f743677d9bc57d783ff1cbdd564862398852b
Kenny McCormick <kennyrkun@users.noreply.github.com>
2024-11-01 11:20:34 -0500

    move git commit php code

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
+<?php
+
+// TODO: show commit on loading page
+if (file_exists(".git"))
+{
+	$refs = file_get_contents(".git/HEAD");
+	$ref = substr($refs, strrpos($refs, "/"), -1); // have to use this to strip a space for some reason
+	$commit = file_get_contents(".git/refs/heads" . $ref); // slash is included in beginning of $ref
+	$commit = htmlspecialchars($commit);
+}
+else
+	$commit = "commit unknown";
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>
-			<?php
-				if (file_exists(".git"))
-				{
-					$refs = file_get_contents(".git/HEAD");
-					$ref = substr($refs, strrpos($refs, "/"), -1); // have to use this to strip a space for some reason
-					$commit = file_get_contents(".git/refs/heads" . $ref); // slash is included in beginning of $ref
-					echo htmlspecialchars($commit);
-				}
-				else
-					echo "commit unknown";
-			?>
-		</title>
+		<title><?= $commit ?></title>
 
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
